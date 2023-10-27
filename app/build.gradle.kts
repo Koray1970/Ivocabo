ba8471a2272dda4803128dc928871988d47787aa
Koray Otağ <korayotag@gmail.com>
2023-10-27 12:39:15 +0300

    202310271239

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,13 +64,13 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.7.2")
-    implementation(platform("androidx.compose:compose-bom:2023.09.01"))
+    implementation("androidx.activity:activity-compose:1.8.0")
+    implementation(platform("androidx.compose:compose-bom:2023.10.01"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3:1.2.0-alpha08")
-    implementation("androidx.hilt:hilt-navigation-compose:1.1.0-alpha01")
+    implementation("androidx.compose.material3:material3:1.2.0-alpha10")
+    implementation("androidx.hilt:hilt-navigation-compose:1.1.0-rc01")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.11")
@@ -78,27 +78,27 @@ dependencies {
     implementation("org.osmdroid:osmdroid-android:6.1.17")
     implementation("com.google.accompanist:accompanist-permissions:0.33.2-alpha")
     implementation("com.google.android.gms:play-services-location:21.0.1")
-    implementation("com.google.android.gms:play-services-maps:18.1.0")
-    implementation(platform("androidx.compose:compose-bom:2023.09.01"))
+    implementation("com.google.android.gms:play-services-maps:18.2.0")
+    implementation(platform("androidx.compose:compose-bom:2023.10.01"))
     implementation("androidx.core:core-splashscreen:1.1.0-alpha02")
-    implementation("androidx.compose.runtime:runtime:1.6.0-alpha06")
-    implementation("androidx.compose.runtime:runtime-livedata:1.6.0-alpha06")
+    implementation("androidx.compose.runtime:runtime:1.6.0-alpha08")
+    implementation("androidx.compose.runtime:runtime-livedata:1.6.0-alpha08")
     implementation("tech.utsmankece:osm-android-compose:0.0.5")
     implementation("androidx.work:work-runtime-ktx:2.8.1")
     //implementation("androidx.compose.runtime:runtime-livedata:1.6.0-alpha06")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.09.01"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.10.01"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.09.01"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.10.01"))
     debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:1.6.0-alpha06")
-    implementation("com.google.dagger:hilt-android:2.48")
-    kapt("com.google.dagger:hilt-android-compiler:2.48")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.6.0-alpha08")
+    implementation("com.google.dagger:hilt-android:2.48.1")
+    kapt("com.google.dagger:hilt-android-compiler:2.48.1")
 
     /*start::room dependency*/
-    val room_version = "2.5.2"
+    val room_version = "2.6.0"
 
     implementation("androidx.room:room-runtime:$room_version")
     annotationProcessor("androidx.room:room-compiler:$room_version")
@@ -113,7 +113,7 @@ dependencies {
 
     /*start:: jetpack navigation*/
     val nav_version = "2.7.3"
-    implementation("androidx.navigation:navigation-compose:2.6.0")
+    implementation("androidx.navigation:navigation-compose:2.7.4")
     //hilt work injection
     implementation("androidx.hilt:hilt-work:1.0.0")
     // When using Kotlin.
